028f4da79e22f2fc606ed5ab3c79501c90286e3c
aimen <artem.igorevich8@rambler.ru>
2024-02-01 17:36:07 +0300

    add logo component

---
+++ components/logo.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+import { headingFont } from '@/lib/fonts';
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="Taskify" width={30} height={30} />
+        <p
+          className={cn('text-lg text-neutral-700 pb-1', headingFont.className)}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+};
