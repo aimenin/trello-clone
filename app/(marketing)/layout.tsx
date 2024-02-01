9460af646c99f47b9c695543022394d6d111ed08
aimen <artem.igorevich8@rambler.ru>
2024-02-01 18:01:05 +0300

    add navbar to marketing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,9 @@
+import { Nabvar } from './_components/navbar';
+
 const MarketingLayout = ({ children }: { childred: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* Header */}
+      <Nabvar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
       {/* Footer */}
     </div>
