cb4ebeec492039c117cf6ed0317076b00c42c281
aimen <artem.igorevich8@rambler.ru>
2024-02-15 18:11:00 +0300

    add first server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,29 @@
+import { db } from '@/lib/db';
+
 const OrganizationPage = () => {
+  async function create(formData: FormData) {
+    'use server';
+
+    const title = formData.get('title') as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
   return (
     <div>
-      <h1>Organization Page</h1>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
     </div>
   );
 };
