d51fcd6912e8543d356ef028a6687d2bbdb274c5
aimen <artem.igorevich8@rambler.ru>
2024-03-06 20:26:10 +0300

    add submit button component

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
+
+import { cn } from '@/lib/utils';
+import { Button, ButtonProps } from '@/components/ui/button';
+
+/**
+ * Component to render a submit button
+ * @param variant - variant of the button (shadcn ui variant)
+ * @param children - children of the button
+ * @param disabled - if the button is disabled
+ * @param className - class name of the button
+ * @returns
+ */
+export const FormSubmit = ({
+  variant,
+  children,
+  disabled,
+  className,
+  ...rest
+}: ButtonProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      disabled={disabled || pending}
+      type="submit"
+      size="sm"
+      className={cn(className)}
+      {...rest}
+    >
+      {children}
+    </Button>
+  );
+};
