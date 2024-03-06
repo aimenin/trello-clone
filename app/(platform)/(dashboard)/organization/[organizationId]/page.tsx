75b1dcad0c6f916c688bc9f348550d8eca9fbcd6
aimen <artem.igorevich8@rambler.ru>
2024-03-06 20:49:06 +0300

    add base of info component for organization

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,9 @@
-import { db } from '@/lib/db';
-import { Form } from './form';
+import { Info } from './_components/info';
 
 const OrganizationPage = async () => {
-  const boards = await db.board.findMany({});
-
   return (
-    <div className="flex flex-col flex-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <div key={board.id}>Board title: {board.title}</div>
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   );
 };
