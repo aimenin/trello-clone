68e6c5f6ea77d24a8599345f12f2be9ef0567f88
aimen <artem.igorevich8@rambler.ru>
2024-03-07 18:15:02 +0300

    add base of form picker component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -15,6 +15,7 @@ import { createBoard } from '@/actions/create-board';
 
 import { FormInput } from './form-input';
 import { FormSubmit } from './form-submit';
+import { FormPicker } from './form-picker';
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -73,6 +74,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form className="space-y-4" action={handleSumbit}>
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
